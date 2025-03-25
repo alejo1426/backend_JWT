32407fad23091e8b61a0d26ff1a8d27ca3eb770c
Alejo U <esteban.bustamanter@uniagustiniana.edu.co>
2025-03-25 13:06:07 -0500

    remove cached env, db , folder

--- db.js
+++
@@ -1,8 +0,0 @@
-// db.js
-const { createClient } = require('@supabase/supabase-js');
-require('dotenv').config();
-
-// Inicializa el cliente de Supabase
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
-
-module.exports = supabase;
